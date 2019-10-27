7ee300387e1e5f2a5b8da7729d460315764025bb
cakoshakib <cakoshakib@gmail.com>
2019-10-27 10:49:14 -0400

    shadows :>

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+
 new fullpage('#fullpage', {
     autoScrolling: true,
     navigation: true,
     navigationTooltips: ['Home', 'Meets','About','Board','Contact'],
     easingcss3: 'ease',
-	easing: 'easeInOutCubic',
+    easing: 'easeInOutCubic',
     fadingEffect:true,
-
-})
+});
