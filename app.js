184a6e3ad4610dc5a340e1990b61c0e3a1a592ee
cakoshakib <cakoshakib@gmail.com>
2019-10-26 22:07:59 -0400

    pretty much finished except for animations

---
+++ app.js
@@ -0,0 +1,9 @@
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: true,
+    navigationTooltips: ['Home', 'Meets','About','Board','Contact'],
+    easingcss3: 'ease',
+	easing: 'easeInOutCubic',
+    fadingEffect:true,
+
+})
